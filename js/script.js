0ab0f9d4b5d58efc3f3479152fd98b705dbba69f
adriano-wb <126106701+adriano-wb@users.noreply.github.com>
2023-04-23 15:46:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -359,11 +359,11 @@ window.onload = function() {
      /* Se todos passarem no teste, avança para o agradecimento */
      if (campoNome.value && /^[a-zA-Z/s]+$/.test(campoNome.value) &&
         campoNumero.value && validNumeroCartao.test(campoNumero.value) &&
-        mesValidade.value && /[0-9]/g.test(mesValidade.value) &&
-        Number(mesValidade.value < 12) && Number(mesValidade.value > 1) &&
-        anoValidade.value && /[0-9]/g.test(anoValidade.value) &&
+        mesValidade.value && /[0-9]{2}/g.test(mesValidade.value) &&
+        !Number(mesValidade.value > 12) && !Number(mesValidade.value < 1) &&
+        anoValidade.value && /[0-9]{2}/g.test(anoValidade.value) &&
         mesValidade.value.length === 2 && anoValidade.value.length === 2 &&
-        /[0-9]/g.test(codigoCvc.value) && codigoCvc.value.length === 3) {
+        /[0-9]{3}/g.test(codigoCvc.value) && codigoCvc.value.length === 3) {
            formSecao.classList.add("animOpacity0");
            formSecao.addEventListener("animationend", () => {
              formSecao.style.display = "none";
