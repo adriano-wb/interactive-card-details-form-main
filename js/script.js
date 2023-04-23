d7953446a6be25468d9aaeef682ac524f3f80b21
adriano-wb <126106701+adriano-wb@users.noreply.github.com>
2023-04-23 15:14:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -357,7 +357,7 @@ window.onload = function() {
      }
  
      /* Se todos passarem no teste, avança para o agradecimento */
-     if (campoNome.value && /[^a-zA-Z0-9\s]/g.test(campoNome.value) &&
+     if (campoNome.value && !/[^a-zA-Z0-9\s]/g.test(campoNome.value) &&
         campoNumero.value && validNumeroCartao.test(campoNumero.value) &&
         mesValidade.value && /[0-9]/g.test(mesValidade.value) &&
         Number(mesValidade.value < 12) && Number(mesValidade.value > 1) &&
